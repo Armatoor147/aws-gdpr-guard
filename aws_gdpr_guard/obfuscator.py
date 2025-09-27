57d80ae919afee7f58b2826b94462c6edd358d35
Armatoor147 <vincent.toor@gmail.com>
2025-09-27 18:22:42 +0200

    Add function obfuscate_file

---
+++ aws_gdpr_guard/obfuscator.py
@@ -0,0 +1,15 @@
+def obfuscate_file(file_to_obfuscate, pii_fields):
+    """
+    Obfuscates file from an S3 bucket.
+
+    Args:
+        file_to_obfuscate: S3 URI of the S3 bucket file.
+        pii_fields: List of column names to obfuscate.
+
+    Returns:
+        Obfuscated CSV as bytes.
+
+    Raises:
+        [Complete this once I know what needs to be raised]
+    """
+    pass
