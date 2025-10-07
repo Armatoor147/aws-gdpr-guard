acf793019af813cfc26b197874836e7ddc0402da
Armatoor147 <vincent.toor@gmail.com>
2025-10-07 16:17:39 +0200

    Make lambda IaC fully functional

--- terraform-lambda-deployment/main.tf
+++ terraform-lambda-deployment/main.tf
@@ -37,8 +37,8 @@ resource "aws_iam_policy" "lambda_s3_policy" {
             Effect = "Allow",
             Action = ["s3:GetObject", "s3:PutObject", "s3: ListBucket"],
             Resource = [
-                "arn:aws:s3:::BUCKEY-NAME", # Replace BUCKET-NAME with a variable
-                "arn:aws:s3:::BUCKET-NAME/*" # Replace BUCKET-NAME with a variable
+                "arn:aws:s3:::${local.bucket_name}",
+                "arn:aws:s3:::${local.bucket_name}/*"
             ]
         }]
     })
@@ -50,6 +50,41 @@ resource "aws_iam_role_policy_attachment" "lambda_s3_attach" {
     policy_arn = aws_iam_policy.lambda_s3_policy.arn
 }
 
+# Create a temporary directory with the correct structure
+resource "null_resource" "prepare_layer" {
+  provisioner "local-exec" {
+    command = <<EOT
+      mkdir -p ${path.module}/layer/python/aws_gdpr_guard
+      cp -r ${path.module}/../aws_gdpr_guard/* ${path.module}/layer/python/aws_gdpr_guard/
+    EOT
+  }
+}
+
+# Zip the custom library
+data "archive_file" "gdpr_guard_layer" {
+    depends_on  = [null_resource.prepare_layer]
+    type        = "zip"
+    source_dir  = "${path.module}/layer/"
+    output_path = "${path.module}/aws_gdpr_guard_layer.zip"
+}
+
+# Create the Lambda layer
+resource "aws_lambda_layer_version" "gdpr_guard_layer" {
+    depends_on       = [data.archive_file.gdpr_guard_layer]
+    layer_name          = "aws_gdpr_guard_layer"
+    description         = "Obfuscator library"
+    filename            = data.archive_file.gdpr_guard_layer.output_path
+    compatible_runtimes = ["python3.12"]
+    source_code_hash    = data.archive_file.gdpr_guard_layer.output_base64sha256
+}
+
+# Zip the Python file dynamically
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "${path.module}/../lambda_function.py"
+  output_path = "${path.module}/lambda_function.zip"
+}
+
 # Lambda function with Pandas layer
 resource "aws_lambda_function" "lambda_function" {
     function_name = "aws-gdpr-guard-lambda"
@@ -57,20 +92,59 @@ resource "aws_lambda_function" "lambda_function" {
     timeout = 60
     handler = "lambda_function.lambda_handler"
     role = aws_iam_role.lambda_role.arn
-
-    filename = "X"
-
-    layers = ["arn:aws:lambda:eu-north-1:336392948345:layer:AWSSDKPandas-Python312:2"]
+    #filename = "lambda_function.zip"
+    filename = data.archive_file.lambda_zip.output_path
+    layers = [
+        "arn:aws:lambda:eu-north-1:336392948345:layer:AWSSDKPandas-Python312:2",
+        aws_lambda_layer_version.gdpr_guard_layer.arn
+    ]
 
     environment {
       variables = {
-        BUCKET_NAME = "X"
+        S3_BUCKET_NAME = local.bucket_name
       }
     }
+    source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 }
+
+# resource "aws_lambda_permission" "trigger" {
+#     statement_id = "AllowExecutionFromS3Bucket"
+#     action = "lambda:InvokeFunction"
+#     function_name = aws_lambda_function.lambda_function.function_name
+#     principal = "s3.amazonaws.com"
+#     source_arn = "arn:aws:s3:::${local.bucket_name}"
+# }
 
 # Output the lambda ARN
 output "lambda_arn" {
     value = aws_lambda_function.lambda_function
 }
 
+variable "S3_bucket_name" {
+    type = string
+    default = "vincent-toor-azorin-aws-gdpr-guard-test-bucket"
+}
+
+variable "S3_bucket_prefix" {
+    type = string
+    default = "aws-gdpr-guard-bucket"
+}
+
+resource "random_id" "S3_bucket_suffix" {
+    byte_length = 4
+}
+
+locals {
+    bucket_name = "${var.S3_bucket_prefix}-${random_id.S3_bucket_suffix.hex}"
+}
+
+resource "aws_s3_bucket" "aws_gdpr_guard_bucket" {
+    bucket = local.bucket_name
+}
+
+resource "aws_s3_object" "dummy_students" {
+    bucket = aws_s3_bucket.aws_gdpr_guard_bucket.bucket
+    key = "dummy_students.csv"
+    source = "${path.module}/../dummy_data/dummy_students.csv"
+    content_type = "text/csv"
+}
