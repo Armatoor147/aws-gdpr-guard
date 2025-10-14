b43081c153cb05283f312aafcd77d0799351678c
Armatoor147 <vincent.toor@gmail.com>
2025-10-14 11:49:38 +0200

    Create local script

---
+++ local_script.py
@@ -0,0 +1,28 @@
+from aws_gdpr_guard.obfuscator import obfuscate_file
+
+
+# Set LOAD_ENVIRONMENT to True if the AWS credentials are strored in a .env file
+# Set LOAD_ENVIRONMENT to False if AWS credentials are exported or configured with `aws configure`
+LOAD_ENVIRONMENT = False
+
+if LOAD_ENVIRONMENT:
+    import os
+    from dotenv import load_dotenv
+
+    load_dotenv()
+    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+
+
+def main():
+    file_to_obfuscate = (
+        "s3://vincent-toor-azorin-aws-gdpr-guard-test-bucket/dummy_students.json"
+    )
+    pii_fields = ["name", "email_address"]
+    data_type = "json"
+    bytes_data = obfuscate_file(file_to_obfuscate, pii_fields, data_type)
+    print(bytes_data)
+
+
+if __name__ == "__main__":
+    main()
