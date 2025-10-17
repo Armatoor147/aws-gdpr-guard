d846e05ec0bde9d1e202ad4ad171ca70888ddab8
Armatoor147 <vincent.toor@gmail.com>
2025-10-17 16:25:23 +0200

    Update EC2 script

--- ec2_script.py
+++ ec2_script.py
@@ -6,7 +6,8 @@ def main():
     S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
     file_to_obfuscate = (f"s3://{S3_BUCKET_NAME}/dummy_students.csv")
     pii_fields = ["name", "email_address"]
-    bytes_data = obfuscate_file(file_to_obfuscate, pii_fields)
+    file_type = "csv"
+    bytes_data = obfuscate_file(file_to_obfuscate, pii_fields, file_type)
     print(bytes_data)
 
 if __name__ == "__main__":
