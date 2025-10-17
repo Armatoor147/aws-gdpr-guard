39d3a0821acff631b273f4f1444ad7c861a47b69
Armatoor147 <vincent.toor@gmail.com>
2025-10-17 16:27:15 +0200

    Update scripts

--- local_script.py
+++ local_script.py
@@ -1,4 +1,5 @@
 from aws_gdpr_guard.obfuscator import obfuscate_file
+import boto3
 
 
 # Set LOAD_ENVIRONMENT to True if the AWS credentials are strored in a .env file
@@ -15,13 +16,33 @@ if LOAD_ENVIRONMENT:
 
 
 def main():
-    file_to_obfuscate = (
-        "s3://vincent-toor-azorin-aws-gdpr-guard-test-bucket/dummy_students.json"
-    )
-    pii_fields = ["name", "email_address"]
-    data_type = "json"
-    bytes_data = obfuscate_file(file_to_obfuscate, pii_fields, data_type)
-    print(bytes_data)
+    # file_to_obfuscate = (
+    #     "s3://vincent-toor-azorin-aws-gdpr-guard-test-bucket/dummy_students.parquet"
+    # )
+    # pii_fields = ["name", "email_address"]
+    # data_type = "parquet"
+    # bytes_data = obfuscate_file(file_to_obfuscate, pii_fields, data_type)
+    # # print(bytes_data)
+    # s3_client = boto3.client("s3")
+    # s3_client.put_object(
+    #     Bucket="vincent-toor-azorin-aws-gdpr-guard-test-bucket",
+    #     Key="obfuscated_dummy_students.parquet",
+    #     Body=bytes_data
+    # )
+
+
+    
+    import pandas as pd
+    import io
+    file_path = "s3://vincent-toor-azorin-aws-gdpr-guard-test-bucket/obfuscated_dummy_students.parquet"
+    bucket_name = "vincent-toor-azorin-aws-gdpr-guard-test-bucket"
+    object_key = "obfuscated_dummy_students.parquet"
+    s3_client = boto3.client("s3")
+    response = s3_client.get_object(Bucket=bucket_name, Key=object_key)
+    data = pd.read_parquet(io.BytesIO(response['Body'].read()))
+    
+    # df = pd.read_parquet(file_path)
+    print(data)
 
 
 if __name__ == "__main__":
