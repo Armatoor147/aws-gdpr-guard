3eec5e7fc19c25935b348ec58351d6c93a0665fa
Armatoor147 <vincent.toor@gmail.com>
2025-10-19 12:28:12 +0200

    Create setup.py for pip install configuration

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aws-gdpr-guard",
+    version="1.0.0",
+    author="Vincent Toor-Azorin",
+    author_email="vincent.toor@gmail.com",
+    description="A Python package for anonymising PII in AWS S3 while ensuring GDPR compliance.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Armatoor147/aws-gdpr-guard",
+    packages=find_packages(),
+    install_requires=[
+        "boto3",
+        "pandas",
+        "pyarrow"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.11",
+)
