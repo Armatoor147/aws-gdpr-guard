459707f461e20d009443da7e0793be0bad860015
Armatoor147 <vincent.toor@gmail.com>
2025-10-19 22:35:37 +0200

    Add 1MB file

---
+++ local_script_for_testing_1MB.py
@@ -0,0 +1,40 @@
+import time
+from aws_gdpr_guard.obfuscator import obfuscate_file
+
+
+# Set LOAD_ENVIRONMENT to True if the AWS credentials are strored in a .env file
+# Set LOAD_ENVIRONMENT to False if AWS credentials are exported or configured with `aws configure`
+LOAD_ENVIRONMENT = False
+
+if LOAD_ENVIRONMENT:
+    import os
+    from dotenv import load_dotenv
+
+    load_dotenv()
+    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+
+
+def main():
+    file_to_obfuscate = "s3://vincent-toor-azorin-aws-gdpr-guard-test-bucket/dummy_students.csv"
+    # file_to_obfuscate = "s3://YOUR_BUCKET_NAME/file_to_obfuscate.csv"
+    pii_fields = ["name", "email_address"]
+    data_type = "csv"
+
+    # Start the timer
+    start_time = time.time()
+
+    bytes_data = obfuscate_file(file_to_obfuscate, pii_fields, data_type)
+
+    # End the timer
+    end_time = time.time()
+
+    # Calculate the elapsed time
+    elapsed_time = end_time - start_time
+    print(f"Obfuscation took {elapsed_time:.2f} seconds")
+
+    print(bytes_data[:1000])
+
+
+if __name__ == "__main__":
+    main()
