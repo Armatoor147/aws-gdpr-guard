505d52935ef03bd7e2e332d7c6cc1d3f996eb63e
Armatoor147 <vincent.toor@gmail.com>
2025-10-19 14:06:10 +0200

    Apply black

--- ec2_script.py
+++ ec2_script.py
@@ -4,11 +4,12 @@ import os
 
 def main():
     S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
-    file_to_obfuscate = (f"s3://{S3_BUCKET_NAME}/dummy_students.csv")
+    file_to_obfuscate = f"s3://{S3_BUCKET_NAME}/dummy_students.csv"
     pii_fields = ["name", "email_address"]
     file_type = "csv"
     bytes_data = obfuscate_file(file_to_obfuscate, pii_fields, file_type)
     print(bytes_data)
+
 
 if __name__ == "__main__":
     main()
--- lambda_function.py
+++ lambda_function.py
@@ -4,7 +4,7 @@ import os
 
 def lambda_handler(event, context):
     S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
-    file_to_obfuscate = (f"s3://{S3_BUCKET_NAME}/dummy_students.csv")
+    file_to_obfuscate = f"s3://{S3_BUCKET_NAME}/dummy_students.csv"
     pii_fields = ["name", "email_address"]
     file_type = "csv"
     bytes_data = obfuscate_file(file_to_obfuscate, pii_fields, file_type)
