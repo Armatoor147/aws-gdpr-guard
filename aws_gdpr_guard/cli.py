93e4f21683ed41f211e31238391658c0c6924315
Armatoor147 <vincent.toor@gmail.com>
2025-10-19 14:58:29 +0200

    Add PII as parser and allow for putObject

--- aws_gdpr_guard/cli.py
+++ aws_gdpr_guard/cli.py
@@ -1,4 +1,5 @@
 import argparse
+import boto3
 from aws_gdpr_guard.obfuscator import obfuscate_file
 
 
@@ -8,11 +9,22 @@ def main():
     parser.add_argument(
         "--output", type=str, help="Output S3 URI for the obfuscated file", default=None
     )
+    parser.add_argument(
+        "--pii-fields", nargs="+", required=True, help="List of PII fields to obfuscate"
+    )
     args = parser.parse_args()
 
     print(f"Obfuscating file at {args.s3_uri}...")
-    obfuscate_file(args.s3_uri, args.output)
-    print("Done!")
+    obfuscated_data = obfuscate_file(args.s3_uri, args.pii_fields)
+    print("Obfuscation complete!")
+
+    if args.output:
+        output_bucket, output_key = args.output.replace("s3://", "").split("/", 1)
+        s3_client = boto3.client("s3")
+        s3_client.put_object(Bucket=output_bucket, Key=output_key, Body=obfuscated_data)
+        print(f"Uploaded obfuscated data to {args.output}")
+    else:
+        print("Obfuscated data (first 100 bytes):", obfuscated_data[:100])
 
 
 if __name__ == "__main__":
